4835c12baa759e74dcc4e4d63418075f49fbf2dc
srinu A <srinivasaddanki.actimize@gmail.com>
2025-11-21 19:20:32 +0530

    implemented navbar

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Ui/Home';
+import MovieDetails from './components/Ui/MovieDetails';
+import Artist from './components/Ui/Artist';
+import NavBar from './components/Ui/NavBar';
+import Payment from './components/Ui/Payment';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<MovieDetails />} />
+        <Route path='/artist' element={<Artist />} />
+        <Route path='/payment' element={<Payment />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
